0d58a58ce9a18eed0aed24bd113bea1e50d98e0b
Sawaraa <daniellakotlar@gmail.com>
2023-08-17 19:07:30 +0300

    third comit

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
-
-  enum Seasons {
-    WINTER, SPRING, SUMMER, FALL
-  }
-
-  enum Months {
-    JANUARY(31, Seasons.WINTER), FEBRUARY (28, Seasons.WINTER),
-    MARCH(31, Seasons.SPRING), APRIL(30, Seasons.SPRING),
-    MAY(31, Seasons.SPRING), JUNE(30, Seasons.SUMMER),
-    JULY(31, Seasons.SUMMER), AUGUST(31, Seasons.SUMMER),
-    SEPTEMBER(30, Seasons.FALL), OCTOBER(31, Seasons.FALL),
-    NOVEMBER(30, Seasons.FALL), DECEMBER(31, Seasons.WINTER);
-    int days;
-    Seasons seasons;
-
-    Months(int days, Seasons seasons) {
-      this.days = days;
-      this.seasons = seasons;
-    }
-
-    public int getDays() {
-      return days;
-    }
-
-    public Seasons getSeasons() {
-      return seasons;
-    }
-  }
+public class Main extends Enum {
 
   static void menu(){
     System.out.println("Натисніть 1, щоб перевірити чи існує такий місяць");
@@ -181,6 +153,7 @@ public class Main {
           if(!ifExist){
             System.out.println("Пора року не існує");
           }
+          break;
         }
 
         case "7" :{
